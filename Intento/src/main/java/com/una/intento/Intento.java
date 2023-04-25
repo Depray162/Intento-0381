ad60d961d91c73f5ac19aa4b72f02f3e5ea5da8f
KevinSalbra <kevinsalazar74.ksb@gmail.com>
2023-04-24 20:10:38 -0600

    ij

--- Intento/src/main/java/com/una/intento/Intento.java
+++ Intento/src/main/java/com/una/intento/Intento.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.una.intento;
 
 /**
@@ -10,8 +9,23 @@ package com.una.intento;
  */
 public class Intento {
 
+    Obj objeto = new Obj();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-         System.out.println("Hello World!");
+       objeto.sumar();
+    }
+
+}
+
+class Obj {
+
+    private int x, y, z, total;
+
+    public String sumar() {
+        x =10;
+        y = x;
+        z = y;
+        total = x + y + z;
+        return "El total de la suma es: " + total;
     }
 }
