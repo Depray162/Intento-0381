39b9b9873b2b2c6ff7d223924149787657a32fce
Depray162 <fabricio1quesada@gmail.com>
2023-04-25 10:50:24 -0600

    Fechas

--- Intento/src/main/java/com/una/intento/Intento.java
+++ Intento/src/main/java/com/una/intento/Intento.java
@@ -3,31 +3,68 @@
  */
 package com.una.intento;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
 /**
  *
  * @author Noe
  */
 public class Intento {
-
-    Obj objeto = new Obj();
-
     public static void main(String[] args) {
-       objeto.sumar(10,10,10);
+        
+        Scanner entradaDatos = new Scanner(System.in);
+        
+       
+        Obj objeto = new Obj();
+        
+        System.out.println(objeto.sumar());
+        //System.out.println("Ingrese la fecha que desea usar");
+       // String fecha1 = entradaDatos.nextLine();
+         
+        Calendar calendario = new GregorianCalendar();
+       Date fecha = new Date();
+       
+       calendario.setTime(fecha);
+       
+       String dateAString = calendario.toString();
+       
+       
+       int year = calendario.get(Calendar.YEAR);
+       int mes = calendario.get(Calendar.MONTH);
+       int dia = calendario.get(Calendar.DATE);
+       
+        System.out.println("Year ="+ year +"/mes= "+ (mes+1) +"/dia= "+  dia );
+         
+        int yearnew = 1983;
+        
+        calendario.set(yearnew, 3, 21);
+        
+        System.out.println("La fecha es: "+calendario.get(Calendar.DATE)+"/"+calendario.get(Calendar.MONTH)+"/"+calendario.get(Calendar.YEAR));
+
     }
 
 }
 
 class Obj {
+    
+    private int x, y, z, total;
 
+    public Obj() {
+    }
+    
+    
+    
+    
     public Obj(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    
-    private int x, y, z, total;
-
+ 
     public String sumar() {
         x =10;
         y = x;
