5c62fc59df0be22ee61d0d89fbe0c698fa9c7d8f
Depray162 <fabricio1quesada@gmail.com>
2023-04-24 19:52:43 -0600

    ffff

---
+++ Intento/src/main/java/com/una/intento/Intento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.una.intento;
+
+/**
+ *
+ * @author Noe
+ */
+public class Intento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         System.out.println("Hello World!");
+    }
+}
